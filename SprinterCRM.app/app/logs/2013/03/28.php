c9e8ae0b1279b4162781aa27235b1c89c86fc28f
Brad Morris <brad@cyber.morris.local>
2013-03-28 19:16:17 +0000

    Initial Commit, Fuel and Skeleton App

---
+++ SprinterCRM.app/app/logs/2013/03/28.php
@@ -0,0 +1,11 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-03-28 19:00:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-03-28 19:00:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-03-28 19:08:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-03-28 19:08:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-03-28 19:10:28 --> Fuel\Core\Fuel::init - The configured locale en_UK is not installed on your system.
+WARNING - 2013-03-28 19:10:28 --> Fuel\Core\Fuel::init - The configured locale en_UK is not installed on your system.
+WARNING - 2013-03-28 19:10:28 --> Fuel\Core\Fuel::init - The configured locale en_UK is not installed on your system.
+WARNING - 2013-03-28 19:10:45 --> Fuel\Core\Fuel::init - The configured locale en_UK is not installed on your system.
+WARNING - 2013-03-28 19:10:45 --> Fuel\Core\Fuel::init - The configured locale en_UK is not installed on your system.
